a8bbb3f449d0a9199ce6b1b88e09c4c002c5295f
emrtreu <merve.turhan@ericsson.com>
2023-10-14 18:55:24 +0200

    build.rs: Respect OUT_DIR set by Cargo when outputting SDRaD shared object
    Co-authored-by: Thomas Nyman  <thomas.nyman@ericsson.com>

--- build.rs
+++ build.rs
@@ -1,15 +1,19 @@
 use std::process::Command;
 use std::path::Path;
+use std::env;
 
 fn main() {
     let sdrad_srcdir = Path::new("./secure-rewind-and-discard/src");
+    let output_prefix = env::var("OUT_DIR").unwrap();
+    let libsdrad_path = Path::new(&output_prefix);
 
     Command::new("sh")
             .arg("-c")
             .arg("make")
+            .env("OUTPUT_PREFIX", &output_prefix)
             .current_dir(&sdrad_srcdir)
             .status().unwrap();
 
-    println!(r"cargo:rustc-link-search={}", &sdrad_srcdir.display());
-    println!(r"cargo:rustc-env=LD_LIBRARY_PATH={}", &sdrad_srcdir.display());
+    println!(r"cargo:rustc-link-search={}", &libsdrad_path.display());
+    println!(r"cargo:rustc-env=LD_LIBRARY_PATH={}", &libsdrad_path.display());
 }
