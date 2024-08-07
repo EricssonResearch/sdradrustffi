905c3a9e2222f96fc9def922815c0f268ae3942d
Jialun Zhang <jjz5354@psu.edu>
2024-08-07 11:59:33 -0400

    fix LD_LIBRARY_PATH

--- build.rs
+++ build.rs
@@ -6,6 +6,7 @@ fn main() {
     let sdrad_srcdir = Path::new("./secure-rewind-and-discard/src");
     let output_prefix = env::var("OUT_DIR").unwrap();
     let libsdrad_path = Path::new(&output_prefix);
+    let original_ld_library_path = env::var("LD_LIBRARY_PATH").unwrap_or_default();
 
     Command::new("git")
             .args(&["checkout", "sdrad_ffi"])
@@ -21,5 +22,5 @@ fn main() {
             .status().unwrap();
 
     println!(r"cargo:rustc-link-search={}", &libsdrad_path.display());
-    println!(r"cargo:rustc-env=LD_LIBRARY_PATH={}", &libsdrad_path.display());
+    println!(r"cargo:rustc-env=LD_LIBRARY_PATH={}:{}", &libsdrad_path.display(), &original_ld_library_path);
 }
